64f30dd1cf9d840d7ae64d45701f8ab40476f54a
VAMAN ACHARYA <17621846+VAMANACHARYA@users.noreply.github.com>
2018-09-29 13:39:21 +0530

    Update FileIO.py

--- Explorer/FileIO.py
+++ Explorer/FileIO.py
@@ -1,28 +1,20 @@
+'''File reading '''
 InputFile = open("Input.txt", "r")
-
 lines = InputFile.readlines()
-results = []
 
+'''Create a list to store reveresed results'''
+results = []
 OutPutFile = open("Output.txt","w")
 def reverse (text):
     lenth = len(text)
-    for i in range(lenth, 0, -1):
-        print text[i-1],
+    for i in range(lenth, -1, -1):
         results.append(text[i-1])
 
-
-
 for i in range(len(lines)):
-    reverse (lines[i])
-    print('\n')
+    reverse(lines[i])
 
 for item in range(len(results)):
-    print results[item],
-    OutPutFile.write(results[item])
+    (OutPutFile.write(results[item]))
 
-print ('\n')
-
-print type(results)
-
-InputFile.close()
 OutPutFile.close()
+InputFile.close()
