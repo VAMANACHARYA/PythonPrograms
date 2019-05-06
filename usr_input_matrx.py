4e122be745953d28c49b135a57f22818b80a1977
VAMAN ACHARYA <vaman.acharya001@gmail.com>
2019-05-06 09:11:12 +0530

    Update usr_input_matrx.py

--- usr_input_matrx.py
+++ usr_input_matrx.py
@@ -2,15 +2,10 @@ rows = int(input("Enter Rows"))
 colmns = int(input("Enter Columns"))
 print (("Rows are {}, columns are {}").format(rows,colmns))
 mat = []
-for i in range (0,rows):       
-    mat.append([])
 for i in range (0,rows):
+    mat.append([])
     for j in range (0,colmns):
         mat[i].append(j)
-        mat[i][j] = 0
-print (mat)
-for i in range (0,rows):
-    for j in range (0,colmns):
         print ("Entry in row: ", i+1, " column: ", j+1)
         mat[i][j] = int (input())
 print (mat)
