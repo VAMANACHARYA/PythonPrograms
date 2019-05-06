5bdbdf2e786720f8a44bb5ede2d8db819c204597
VAMAN ACHARYA <vaman.acharya001@gmail.com>
2019-05-06 09:04:17 +0530

    Update usr_input_matrx.py

--- usr_input_matrx.py
+++ usr_input_matrx.py
@@ -10,7 +10,7 @@ for i in range (0,rows):
         mat[i][j] = 0
 print (mat)
 for i in range (0,rows):
-    for j in range (0,n):
+    for j in range (0,colmns):
         print ("Entry in row: ", i+1, " column: ", j+1)
         mat[i][j] = int (input())
 print (mat)
